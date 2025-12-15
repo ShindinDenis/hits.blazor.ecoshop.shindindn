ca0493f7ac51533d3e561de25bad69a529894f2d
Denis <nikolaj.shindin61@gmail.com>
2025-12-15 12:45:22 +0500

    Fix

--- EcoShop/obj/Debug/net9.0/EcoShop.AssemblyInfo.cs
+++ EcoShop/obj/Debug/net9.0/EcoShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e1c77fb722f613ebdddedc347ef8724c6c7d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
