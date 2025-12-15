29fd005dde3857942b85762fdc6e71c521eeb247
Denis <nikolaj.shindin61@gmail.com>
2025-12-15 14:01:05 +0500

    bootstrap fix

--- EcoShop/obj/Debug/net9.0/EcoShop.AssemblyInfo.cs
+++ EcoShop/obj/Debug/net9.0/EcoShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d264aa507c91848a4011f415a5d7f20b027faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41814d673c50d97935c6938b314394150239b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
